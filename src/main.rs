de0be7d1f0cb13c7a31ce4ba50317a98d4df6af7
Zakarias Viinikka <zakariasviinikka@gmail.com>
2026-05-12 08:02:20 +0200

    s

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::fs::File;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    let mut file = File::create("./src/output.txt").unwrap();
+    writeln!(file, "First 1asdds").unwrap();
+    writeln!(file, "Second 2asdads: {}", 42).unwrap();
 }
